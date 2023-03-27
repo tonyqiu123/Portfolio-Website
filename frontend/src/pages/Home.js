b2041a227d8aad5b85e1f0cff2fe3978dd164956
tonyqiu123 <tonyqiu12345@gmail.com>
2023-03-27 19:49:28 -0400

    updated pictures for projects in home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,10 +5,6 @@ import AnimatedPage from '../components/AnimatedPage'
 import HomeGrid from '../components/HomeGrid'
 import linkedin from '../images/linkedin.svg'
 import instagram from '../images/instagram.svg'
-import brightbearsinfo from '../images/brightbearsinfo.png'
-import friendaccounting from '../images/friendaccounting.png'
-import tonyswebdesigns from '../images/tonyswebdesigns.png'
-import jobportal from '../images/jobportal.png'
 
 export default function Home() {
 
@@ -34,13 +30,13 @@ export default function Home() {
           <div className='grid-ImgCover' style={{ backgroundImage: `url("https://cdn.discordapp.com/attachments/715319623637270638/1090022543819280384/fanart__link_click___cheng_xiaoshi_by_synziac_df11i8g-fullview.png")`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
         </div>
 
-        <HomeGrid name="Modern Job Portal" tools={["React", "Node", "MongoDB", "CSS"]} span="6" gitLink='https://github.com/tonyqiu123/Job-Portal' url='https://modernjobportal.netlify.app/' figmaLink='https://www.figma.com/file/mk4LqpyZw00jnbUyKX3FZR/Company-Job-Portal?node-id=0%3A1&t=dEgCxOKD9R4leCxV-1' bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1089359182517514351/Group_18.png', `${jobportal}`]} delay='4.5'/>
+        <HomeGrid name="Modern Job Portal" tools={["React", "Node", "MongoDB", "CSS"]} span="6" gitLink='https://github.com/tonyqiu123/Job-Portal' url='https://modernjobportal.netlify.app/' figmaLink='https://www.figma.com/file/mk4LqpyZw00jnbUyKX3FZR/Company-Job-Portal?node-id=0%3A1&t=dEgCxOKD9R4leCxV-1' bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1090059298102575215/Group_26.png', 'https://cdn.discordapp.com/attachments/715319623637270638/1090058355399204975/Group_19.png']} delay='4.5'/>
 
-        <HomeGrid name="BrightBearsInfo" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink='' url='https://brightbearsinfo.com' figmaLink='https://www.figma.com/file/CkkDvDIjMz5zNx7dsbXJOx/BrightBearsInfo?node-id=0-1&t=rXzuYUu0BHHqkOr9-0' bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1089359713327661206/Frame_137.png', `${brightbearsinfo}`]} delay='3' />
+        <HomeGrid name="BrightBearsInfo" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink='' url='https://brightbearsinfo.com' figmaLink='https://www.figma.com/file/CkkDvDIjMz5zNx7dsbXJOx/BrightBearsInfo?node-id=0-1&t=rXzuYUu0BHHqkOr9-0' bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1090058355663458325/Group_20.png', 'https://cdn.discordapp.com/attachments/715319623637270638/1090058355940270160/Group_21.png']} delay='3' />
 
-        <HomeGrid bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1089359018956435578/Frame_139.png', `${friendaccounting}`]} name="Friend Accounting" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink="https://github.com/tonyqiu123/friend-accounting" url='https://friendaccounting.com/en/index' delay='4' />
+        <HomeGrid bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1090058356259045376/Group_22.png', 'https://cdn.discordapp.com/attachments/715319623637270638/1090058356510707914/Group_23.png']} name="Friend Accounting" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink="https://github.com/tonyqiu123/friend-accounting" url='https://friendaccounting.com/en/index' delay='4' />
 
-        <HomeGrid bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1089359019858210826/Frame_141.png',`${tonyswebdesigns}`]} name="Tony's Web Designs" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink="https://github.com/tonyqiu123/tonys-web-designs.ca" url='https://tonyswebdesigns.ca/' delay='3.5' />
+        <HomeGrid bgImgs={['https://cdn.discordapp.com/attachments/715319623637270638/1090058356791713822/Group_24.png','https://cdn.discordapp.com/attachments/715319623637270638/1090058357093711983/Group_25.png']} name="Tony's Web Designs" tools={["HTML", "CSS", "JavaScript"]} span="6" gitLink="https://github.com/tonyqiu123/tonys-web-designs.ca" url='https://tonyswebdesigns.ca/' delay='3.5' />
 
 
       </div>
