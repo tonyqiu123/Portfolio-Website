f88c0f8314ac4e99b5d44a6dc2ac7b3e4ddb9d1c
tonyqiu123 <tonyqiu12345@gmail.com>
2023-09-19 13:39:52 -0400

    added tracker file to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,22 @@ function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement('script');
+    
+    // Set the script source to your web analytics tracker URL
+    script.src = 'https://web-analytics-83e1.vercel.app/tracker.js';
+    
+    // Add the script to the document's head
+    document.head.appendChild(script);
+    
+    // Clean up by removing the script when the component unmounts
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       <SideNavBar/>
